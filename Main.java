218d131b414b04a4a9c6a82900250a22049cb75b
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-09-17 23:30:07 -0400

    strings para obter score

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        ScoreClient client = new CachedClient(new ControlledClient(new APIClient()));
+
+        String[] cpfs = { "07527868147", "12345678909", "98765432100"};
+
+        for (String cpf : cpfs) {
+            System.out.println("Score for CPF " + cpf + ": " + client.getScore(cpf));
+        }
+    }
+}
